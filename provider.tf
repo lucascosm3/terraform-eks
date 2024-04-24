124756cd7ed70fc05359685fd32ec4763fc3e90a
lucascosm3 <lcds1944@gmail.com>
2024-04-23 22:53:41 -0300

    fix: lint

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-eks-remote-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 # Configure the AWS Provider
