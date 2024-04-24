b4296d0316d0c1b472f9045840abaad7c6e9f441
lucascosm3 <lcds1944@gmail.com>
2024-04-24 09:05:05 -0300

    feat: add tag for elb

--- public.tf
+++ public.tf
@@ -1,21 +1,23 @@
 resource "aws_subnet" "public-1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "terraform-eks-subnet-1a"
+    Name                     = "terraform-eks-subnet-1a"
+    "kubernetes.io/role/elb" = 1
   }
 }
 
 resource "aws_subnet" "public-1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "terraform-eks-subnet-1b"
+    Name                     = "terraform-eks-subnet-1b"
+    "kubernetes.io/role/elb" = 1
   }
 }
