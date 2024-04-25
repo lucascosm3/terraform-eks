f211492f8c8698dcaa70eb221376327d1a1b90d9
lucascosm3 <lcds1944@gmail.com>
2024-04-25 08:24:32 -0300

    feat: create variable to be used (Name tag) project

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
 }
+
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources (Name tag)"
+
+}
