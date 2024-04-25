7c4910a4358b823415dd008e0e577708b29ecee0
lucascosm3 <lcds1944@gmail.com>
2024-04-25 08:02:07 -0300

    refactor: rename subnets

--- public.tf
+++ public.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "public-1a" {
+resource "aws_subnet" "eks_subnet_public-1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
   availability_zone       = "${data.aws_region.current.name}a"
@@ -13,7 +13,7 @@ resource "aws_subnet" "public-1a" {
   )
 }
 
-resource "aws_subnet" "public-1b" {
+resource "aws_subnet" "eks_subnet_public-1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone       = "${data.aws_region.current.name}b"
