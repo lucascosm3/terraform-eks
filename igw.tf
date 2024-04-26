7c528809522edb8614c77b905e233b4e4713a653
lucascosm3 <lcds1944@gmail.com>
2024-04-26 07:14:58 -0300

    feat: public route table

---
+++ igw.tf
@@ -0,0 +1,26 @@
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eks_igw"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_public_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-pub-route-table"
+    }
+  )
+}
--- public.tf
+++ public.tf
@@ -27,3 +27,13 @@ resource "aws_subnet" "eks_subnet_public-1b" {
     }
   )
 }
+
+resource "aws_route_table_association" "eks_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public-1a.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public-1b.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
