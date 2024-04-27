93f500516b1169f5ef70c4c5fddeceadab16525b
lucascosm3 <lcds1944@gmail.com>
2024-04-27 14:52:11 -0300

    feat: output subnets

---
+++ modules/network/output.tf
@@ -0,0 +1,15 @@
+output "subnet_pub_1a" {
+  value = aws_subnet.eks_subnet_public-1a.id
+}
+
+output "subnet_pub_1b" {
+  value = aws_subnet.eks_subnet_public-1b.id
+}
+
+output "subnet_priv_1a" {
+  value = aws_subnet.eks_subnet_private-1a
+}
+
+output "subnet_priv_1b" {
+  value = aws_subnet.eks_subnet_private-1b
+}
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "subnet_pub_1a" {
+  value = module.eks_network.subnet_pub_1a
+}
