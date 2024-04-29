f13ff8173dc5cc2f8bf37bd39cd3e8c3eb1d1037
lucascosm3 <lcds1944@gmail.com>
2024-04-29 08:00:22 -0300

    feat: attachment eks-cluster-role

--- modules.tf
+++ modules.tf
@@ -4,3 +4,9 @@ module "eks_network" {
   project_name = var.project_name
   tags         = local.tags
 }
+
+module "eks_cluster" {
+  source       = "./modules/cluster"
+  project_name = var.project_name
+  tags         = local.tags
+}
