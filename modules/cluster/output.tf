e528c1dcbadf0698a98a993a53a46e475d953e2f
lucascosm3 <lcds1944@gmail.com>
2024-04-30 07:41:51 -0300

    feat: debug sg-rules

---
+++ modules/cluster/output.tf
@@ -0,0 +1,4 @@
+/*output "eks_vpc_config" {
+  value = aws_eks_cluster.eks_cluster.vpc_config
+}
+*/
--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-/*output "subnet_pub_1a" {
-  value = module.eks_network.subnet_pub_1a
+/*output "eks_vpc_config" {
+  value = module.eks_cluster.eks_vpc_config
 }
 */
