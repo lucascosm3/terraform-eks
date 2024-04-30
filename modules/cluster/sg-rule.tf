0d4c553e852f97ef2de9f2b3e90d9c90bbc4cc14
lucascosm3 <lcds1944@gmail.com>
2024-04-30 07:50:13 -0300

    feat: created sg-rule for cluster

---
+++ modules/cluster/sg-rule.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group_rule" "eks_cluster_sg_rule" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  security_group_id = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+}
