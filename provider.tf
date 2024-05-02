e8f6fa9d0b44e0e84209465dea17d6873fe61232
lucascosm3 <lcds1944@gmail.com>
2024-05-02 08:54:09 -0300

    feat: adding kubernetes provider

--- provider.tf
+++ provider.tf
@@ -4,7 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.29.0"
+    }
+
   }
+
   backend "s3" {
     bucket = "terraform-eks-remote-state"
     key    = "dev/terraform.tfstate"
@@ -15,4 +21,15 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+}
+
+#Configure the Kubernetes
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
