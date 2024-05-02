820f37b61124993abe864ff57aec055162b4fd28
lucascosm3 <lcds1944@gmail.com>
2024-05-02 08:53:38 -0300

    feat: testing output kubernetes

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "oidc" {
   value = module.eks_cluster.oidc
 }
+
+output "certificate_authority" {
+  value = module.eks_cluster.certificate_authority
+}
+
+output "cluster_endpoint" {
+  value = module.eks_cluster.cluster_endpoint
+}
