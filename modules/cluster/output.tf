3b79b36960288040cf32846862db7a4cb90117df
lucascosm3 <lcds1944@gmail.com>
2024-05-02 08:53:02 -0300

    feat: adding output kubernetes provider

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,8 +1,15 @@
 output "cluster_name" {
   value = aws_eks_cluster.eks_cluster.id
-
 }
 
 output "oidc" {
   value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+output "certificate_authority" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
